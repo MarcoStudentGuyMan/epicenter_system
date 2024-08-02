8ffd452470943d39b3183cb3194a119c21a87ea7
CHEY2x <jjpamigo@su.edu.ph>
2024-08-02 20:06:53 +0800

    FIX ROUTER

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
-import './router';
+import LoginA from './Admin/login_a'; // Make sure the path is correct
+import LoginT from './Tenant/login_t'; // Make sure the path is correct
 
+function NavigationComponent() {
+    const navigate = useNavigate();
 
-function App() {
-  const navigate = useNavigate();//declaring navigate
-
-
-  return (
-    <div className="App">
-      
-      <p>SELECT USER LOGIN TO EPICENTER</p>
-      <div class="container">
-        <div class="square" onclick={()=> navigate('/login_admin')}>ADMIN</div>
-        <div class="square" onclick={()=> navigate('/login_tenant')}>TENANT</div>
-      </div>
-
-   </div>
-  );
+    return (
+        <>
+            <p>SELECT USER LOGIN TO EPICENTER</p>
+            <div className="container">
+                <div className="square" onClick={() => navigate('/login_admin')}>ADMIN</div>
+                <div className="square" onClick={() => navigate('/login_tenant')}>TENANT</div>
+            </div>
+        </>
+    );
 }
 
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<NavigationComponent />} />
+                    <Route path="/login_admin" element={<LoginA />} />
+                    <Route path="/login_tenant" element={<LoginT />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
 
 export default App;
