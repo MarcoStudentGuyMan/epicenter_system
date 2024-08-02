daa2520a60d49a86e7a297bd2af1c0693222e40e
Jazel Equia <equiajazel@gmail.com>
2024-08-02 14:54:07 +0800

    DEBUG: it's about dependencies

---
+++ src/Admin/login_a.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import './Admin.css';
+
+function LoginA() {
+    console.log("Rendering LoginA Component");
+    return <p className="logo">Hi, this is admin</p>;
+  }
+  
+  export default LoginA;
+  
