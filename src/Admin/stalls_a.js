160d04b083b827fa9f162d428a4a86a519bee5fa
Jazel Equia <equiajazel@gmail.com>
2024-08-23 21:40:12 +0800

    stall commit to jaznew

--- src/Admin/stalls_a.js
+++ src/Admin/stalls_a.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { IonToggle, IonIcon, IonBreadcrumbs, IonBreadcrumb, IonButtons, IonButton } from '@ionic/react';
 import { useNavigate } from 'react-router-dom';
 import Select from 'react-select';
-import { easel,notifications, personCircle,pencil, trash, storefront, people, triangle, prism,home, mail, chatbubble, newspaper, calculator, exit } from 'ionicons/icons';
+import { easel,notifications, personCircle,pencil,cube, trash, storefront, people, triangle, prism,home, mail, chatbubble, newspaper, calculator, exit } from 'ionicons/icons';
 import '../styles/stallA.css'; 
 import '../styles/Stall.css'; 
 import Switch from '@mui/material/Switch';
@@ -43,31 +43,32 @@ function Sidebar() {
                 />
             </div>
             <div className="sidebar-content">
-                <nav>
+            <nav>
                     <ul>
-                        <li><span>Hello (user)</span> </li>
+                        <li><span style={{ fontSize: '18px', marginRight: '5px' }}>Hello (user)</span> </li>
                         <li className="title"><span>Home</span></li>
-                        <li><IonIcon icon={easel} /><span><a onClick={() => navigate('/dashboard_admin')}>Dashboard</a></span></li>
+                        <li><IonIcon icon={easel} style={{ fontSize: '18px', marginRight: '5px' }} /><span><a onClick={() => navigate('/dashboard_admin')}>Dashboard</a></span></li>
 
                         <li className="title"><span>Account</span></li>
-                        <li><IonIcon icon={personCircle} /> <span><a onClick={() => navigate('/profile_admin')}>Profile</a></span></li>
+                        <li><IonIcon icon={personCircle}style={{ fontSize: '18px', marginRight: '5px' }} /> <span><a onClick={() => navigate('/profile_admin')}>Profile</a></span></li>
 
                         <li className="title"><span>Environment</span></li>
-                        <li><IonIcon icon={storefront} /> <span><a onClick={() => navigate('/stall_admin')}>Stalls</a></span></li>
-                        <li><IonIcon icon={people} /> <span><a onClick={() => navigate('/tenant_admin')}>Tenants</a></span></li>
-
+                        <li><IonIcon icon={cube} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/unit_stall_admin')}>Stall Units</a></span></li>
+                        <li><IonIcon icon={storefront} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/stall_admin')}>Stalls</a></span></li>
+                        <li><IonIcon icon={people} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/tenant_admin')}>Tenants</a></span></li>
+                        
                         <li className="title"><span>Website Customization</span></li>
-                        <li><IonIcon icon={triangle} /> <span><a onClick={() => navigate('/epicentersite_admin')}>Epicenter Site</a></span></li>
-                        <li><IonIcon icon={prism} /> <span><a onClick={() => navigate('/minisite_admin')}>Mini Sites</a></span></li>
+                        <li><IonIcon icon={triangle} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/epicentersite_admin')}>Epicenter Site</a></span></li>
+                        <li><IonIcon icon={prism} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/minisite_admin')}>Mini Sites</a></span></li>
 
                         <li className="title"><span>Communication</span></li>
-                        <li><IonIcon icon={mail} /> <span><a onClick={() => navigate('/email_admin')}>Email</a></span></li>
-                        <li><IonIcon icon={chatbubble} /> <span><a onClick={() => navigate('/message_admin')}>Message</a></span></li>
+                        <li><IonIcon icon={mail} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/email_admin')}>Email</a></span></li>
+                        <li><IonIcon icon={chatbubble} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/message_admin')}>Message</a></span></li>
 
                         <li className="title"><span>Rent Information</span></li>
-                        <li><IonIcon icon={newspaper} /> <span><a onClick={() => navigate('/rentbalance_admin')}>Rent Balance</a></span></li>
-                        <li><IonIcon icon={calculator} /> <span><a onClick={() => navigate('/rentautomation_admin')}>Rent Automation</a></span></li>
-                        <li><IonIcon icon={exit} /> <span><a onClick={() => navigate('/loginHere')}>Logout</a></span></li>
+                        <li><IonIcon icon={newspaper} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/rentbalance_admin')}>Rent Balance</a></span></li>
+                        <li><IonIcon icon={calculator} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/rentautomation_admin')}>Rent Automation</a></span></li>
+                        <li><IonIcon icon={exit} style={{ fontSize: '18px', marginRight: '5px' }}/> <span><a onClick={() => navigate('/login_admin')}>Logout</a></span></li>
                     </ul>
                 </nav>
             </div>
@@ -79,6 +80,7 @@ function StallA() {
     const navigate = useNavigate();
     const [selectedStalls, setSelectedStalls] = useState([]);
     const [data, setData] = useState([]);
+    
 
     useEffect(() => {
         const fetchData = async () => {
@@ -112,6 +114,18 @@ function StallA() {
         setSelectedStalls(selectedOptions);
     };
 
+
+    const stallOptions = [
+        { value: '1A', label: <span className="black-text">1A</span> },
+        { value: '1B', label: <span className="black-text">1B</span> },
+        { value: '1C', label: <span className="black-text">1C</span> },
+        { value: '1D', label: <span className="black-text">1D</span> },
+        { value: '1E', label: <span className="black-text">1E</span> },
+        // Add more options as needed
+    ];
+
+  
+
     return (
         <div className="app-container">
             <Sidebar />
@@ -126,6 +140,54 @@ function StallA() {
                     </IonBreadcrumb>
                     <IonBreadcrumb>Stalls</IonBreadcrumb>
                 </IonBreadcrumbs>
+
+                <div className="stall-form">
+                        <div className="form-group">
+                            <label>Business Name:</label>
+                            <input placeholder="Enter Business Name" />
+                        </div>
+                        <div className="form-group">
+                            <label>Business Description:</label>
+                            <input placeholder="Enter Business Description" />
+                        </div>
+                        
+                        <div className="form-group">
+                            <label>Tenant ID:</label>
+                            <select>
+                                <option value="" disabled selected>Select Tenant ID</option>
+                                <option>Sample Tenant ID</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label>Stall Type:</label>
+                            <select>
+                                <option value="" disabled selected>Select Stall Type</option>
+                                <option>Cafe and Pastry</option>
+                                <option>Restaurant and Bar</option>
+                                <option>Sweets and Desserts</option>
+                                <option>Groceries</option>
+                                <option>Others</option>
+                            </select>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Stall Unit/s:</label>
+                            <Select 
+                                isMulti
+                                options={stallOptions}
+                                onChange={handleStallChange}
+                                value={selectedStalls}
+                                classNamePrefix="react-select"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Business Logo:</label>
+                            <div className="business-logo-field">
+                                <input type="file" />
+                                <button>Add</button>
+                            </div>
+                        </div>
+                    </div>
 
                 <section className="profile-Align">
                     <table className="stalls-table">
